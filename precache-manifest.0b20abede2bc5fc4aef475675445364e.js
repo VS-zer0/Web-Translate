7398af14f21f8bbff957f9d86f85b6836ff54052
VS-zer0 <78421895+VS-zer0@users.noreply.github.com>
2021-12-15 18:02:48 +0300

    Add files via upload

---
+++ precache-manifest.0b20abede2bc5fc4aef475675445364e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "355102cb12c76928384f8cb0f800de3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b028fced544fc71a00e9",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "8ae49aac9dab507a9ba8",
+    "url": "/static/js/2.18b2a2a8.chunk.js"
+  },
+  {
+    "revision": "b028fced544fc71a00e9",
+    "url": "/static/js/main.8850ece0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
